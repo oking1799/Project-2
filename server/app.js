b1f67eda1f168f675cfc72c6ea0b0a922d076325
Owen King <oking1799@gmail.com>
2020-11-03 13:58:42 -0500

    domomaker c

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const redis = require('redis');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -24,12 +28,28 @@ mongoose.connect(dbURL, mongooseOptions, (err) => {
     throw err;
   }
 });
+let redisURL = {
+  hostname: 'redis-15770.c246.us-east-1-4.ec2.cloud.redislabs.com',
+  port: '15770',
+};
+
+let redisPASS = 'BwxIDQpyJj0lOM3RArR25vyFBswVQChL';
+if(process.env.REDISCLOUD_URL){
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPASS] = redisURL.auth.split(':');
+}
+let redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
+});
 
 const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -44,6 +64,26 @@ app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+app.use(session({
+  key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized : true,
+  cookie: {
+    httpOnly: true,
+  }
+}));
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if(err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  console.log('Missing CSRF token!');
+  return false;
+});
 
 router(app);
 
