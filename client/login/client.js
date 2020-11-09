b70d207d38818ea0c6641de2c51190f0688bd9d4
Owen King <oking1799@gmail.com>
2020-11-09 14:44:12 -0500

    bruh

--- client/login/client.js
+++ client/login/client.js
@@ -32,7 +32,7 @@ const handleSignup = (e) => {
 
     
 
-    sendAjax('POST', $("#signupForm").attr("action"), $("signupForm").serialize(), redirect);
+    //sendAjax('POST', $("#signupForm").attr("action"), $("signupForm").serialize(), redirect);
 
     return false;
 };
