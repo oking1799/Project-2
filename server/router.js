daa126592af6792915d5f036724affb249c08566
Owen King <oking1799@gmail.com>
2020-11-18 18:41:56 -0500

    working

--- server/router.js
+++ server/router.js
@@ -3,13 +3,13 @@ const mid = require('./middleware');
 
 const router = (app) => {
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
-  // app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
+  app.get('/getLocations', mid.requiresLogin, controllers.Location.getLocations);
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
-  app.get('/map', mid.requiresLogin, controllers.Domo.makerPage);
-  app.post('/map', mid.requiresLogin, controllers.Domo.make);
+  app.get('/map', mid.requiresLogin, controllers.Location.mapPage);
+  app.post('/map', mid.requiresLogin, controllers.Location.newLocation);
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
