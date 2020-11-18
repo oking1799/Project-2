b4b5697bfbe56cc5c71bfe477e120ba4a65d9cc0
Owen King <oking1799@gmail.com>
2020-11-18 18:54:09 -0500

    working

---
+++ hosted/bundle.js
@@ -0,0 +1,92 @@
+"use strict";
+
+var MapPage = function MapPage(props) {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Map Marker API"), /*#__PURE__*/React.createElement("div", {
+    id: "map"
+  }), /*#__PURE__*/React.createElement("form", {
+    id: "locationForm",
+    action: "/addLocation",
+    method: "post"
+  }, /*#__PURE__*/React.createElement("label", {
+    "for": "locationName"
+  }, "name: "), /*#__PURE__*/React.createElement("input", {
+    id: "nameField",
+    type: "text",
+    name: "name",
+    value: "uluru"
+  }), /*#__PURE__*/React.createElement("label", {
+    "for": "latitide"
+  }, "Latitude: "), /*#__PURE__*/React.createElement("input", {
+    id: "latitudeField",
+    type: "float",
+    name: "latitude",
+    value: "-25.344"
+  }), /*#__PURE__*/React.createElement("label", {
+    "for": "longitude"
+  }, "Longitude "), /*#__PURE__*/React.createElement("input", {
+    id: "longitudeField",
+    type: "float",
+    name: "longitude",
+    value: "131.036"
+  }), /*#__PURE__*/React.createElement("label", {
+    "for": "rating"
+  }, "Rating "), /*#__PURE__*/React.createElement("input", {
+    id: "ratingField",
+    type: "number",
+    min: "0",
+    max: "5",
+    step: "1",
+    name: "rating",
+    value: "5"
+  }), /*#__PURE__*/React.createElement("label", {
+    "for": "review"
+  }, "Review "), /*#__PURE__*/React.createElement("input", {
+    id: "reviewField",
+    type: "text",
+    name: "review",
+    value: "Biggest rock I ever did see!"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "submit",
+    value: "Add Location"
+  })), /*#__PURE__*/React.createElement("form", {
+    id: "locationFormGet",
+    action: "/getLocation",
+    method: "get"
+  }, /*#__PURE__*/React.createElement("label", {
+    "for": "query"
+  }, "Search for a Specific Location"), /*#__PURE__*/React.createElement("input", {
+    id: "queryField",
+    type: "text",
+    name: "query"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "submit",
+    value: "Get Location",
+    id: "getButton"
+  }), /*#__PURE__*/React.createElement("select", {
+    id: "methodSelect"
+  }, /*#__PURE__*/React.createElement("option", {
+    value: "get"
+  }, "GET"), /*#__PURE__*/React.createElement("option", {
+    value: "head"
+  }, "HEAD"))), /*#__PURE__*/React.createElement("section", {
+    id: "content"
+  }));
+};
+
+var locationList = function locationList(props) {
+  if (props.locations.length === 0) {
+    return /*#__PURE__*/React.createElement("div", {
+      className: "locationList"
+    }, /*#__PURE__*/React.createElement("h3", {
+      className: "noLocation"
+    }, "No Locations yet"));
+  }
+
+  var locationNodes = props.locations.map(function (location) {
+    //console.log("talent" + domo.talent)
+    return /*#__PURE__*/React.createElement("div", null);
+  });
+  return /*#__PURE__*/React.createElement("div", {
+    className: "locationList"
+  }, locationNodes);
+};
---
+++ hosted/loginBundle.js
@@ -0,0 +1,149 @@
+"use strict";
+
+var handleLogin = function handleLogin(e) {
+  e.preventDefault();
+  $("#domoMessage").animate({
+    width: 'hide'
+  }, 350);
+
+  if ($("#user").val() == '' || $("#pass").val() == '') {
+    handleError("RAWR! Username or password is empty");
+    return false;
+  }
+
+  console.log($("input[name=_csrf]").val());
+  sendAjax('POST', $("#loginForm").attr("action"), $("#loginForm").serialize(), redirect);
+  return false;
+};
+
+var handleSignup = function handleSignup(e) {
+  e.preventDefault();
+  $("#domoMessage").animate({
+    width: 'hide'
+  }, 350);
+
+  if ($("#user").val() == '' || $("#pass").val() == '' || $("#pass2").val() == '') {
+    handleError("All fields are required");
+    return false;
+  }
+
+  if ($("#pass").val() !== $("#pass2").val()) {
+    handleError("Passwords do not match!");
+    return false;
+  }
+
+  console.log("serialized:" + $("signupForm").serialize());
+  sendAjax('POST', $("#signupForm").attr("action"), $("#signupForm").serialize(), redirect);
+  return false;
+};
+
+var LoginWindow = function LoginWindow(props) {
+  return /*#__PURE__*/React.createElement("form", {
+    id: "loginForm",
+    name: "loginForm",
+    onSubmit: handleLogin,
+    action: "/login",
+    method: "POST",
+    className: "mainForm"
+  }, /*#__PURE__*/React.createElement("label", {
+    htmlFor: "username"
+  }, "Username: "), /*#__PURE__*/React.createElement("input", {
+    id: "user",
+    type: "text",
+    name: "username",
+    placeholder: "username"
+  }), /*#__PURE__*/React.createElement("label", {
+    htmlFor: "pass"
+  }, "Password: "), /*#__PURE__*/React.createElement("input", {
+    id: "pass",
+    type: "password",
+    name: "pass",
+    placeholder: "password"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "hidden",
+    name: "_csrf",
+    value: props.csrf
+  }), /*#__PURE__*/React.createElement("input", {
+    className: "formSubmit",
+    type: "submit",
+    value: "Sign in"
+  }));
+};
+
+var SignupWindow = function SignupWindow(props) {
+  return /*#__PURE__*/React.createElement("form", {
+    id: "signupForm",
+    name: "signupForm",
+    onSubmit: handleSignup,
+    action: "/signup",
+    method: "POST",
+    className: "mainForm"
+  }, /*#__PURE__*/React.createElement("label", {
+    htmlFor: "username"
+  }, "Username: "), /*#__PURE__*/React.createElement("input", {
+    id: "user",
+    type: "text",
+    name: "username",
+    placeholder: "username"
+  }), /*#__PURE__*/React.createElement("label", {
+    htmlFor: "pass"
+  }, "Password: "), /*#__PURE__*/React.createElement("input", {
+    id: "pass",
+    type: "password",
+    name: "pass",
+    placeholder: "password"
+  }), /*#__PURE__*/React.createElement("label", {
+    htmlFor: "pass2"
+  }, "Password: "), /*#__PURE__*/React.createElement("input", {
+    id: "pass2",
+    type: "password",
+    name: "pass2",
+    placeholder: "retype password"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "hidden",
+    name: "_csrf",
+    value: props.csrf
+  }), /*#__PURE__*/React.createElement("input", {
+    className: "formSubmit",
+    type: "submit",
+    value: "Sign up"
+  }));
+};
+
+var createLoginWindow = function createLoginWindow(csrf) {
+  ReactDOM.render( /*#__PURE__*/React.createElement(LoginWindow, {
+    csrf: csrf
+  }), document.querySelector("#content"));
+};
+
+var createSignupWindow = function createSignupWindow(csrf) {
+  ReactDOM.render( /*#__PURE__*/React.createElement(SignupWindow, {
+    csrf: csrf
+  }), document.querySelector("#content"));
+};
+
+var setup = function setup(csrf) {
+  var loginButton = document.querySelector("#loginButton");
+  var signupButton = document.querySelector("#signupButton");
+  signupButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    createSignupWindow(csrf);
+    return false;
+  });
+  loginButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    createLoginWindow(csrf);
+    return false;
+  });
+  createLoginWindow(csrf);
+};
+
+var getToken = function getToken() {
+  sendAjax('GET', '/getToken', null, function (result) {
+    setup(result.csrfToken);
+  });
+};
+
+$(document).ready(function () {
+  getToken();
+});
