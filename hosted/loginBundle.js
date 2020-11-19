f18f85e26d3b5769d23f202a6634d8858f578ace
Owen King <oking1799@gmail.com>
2020-11-19 17:45:00 -0500

    haha yes

--- hosted/loginBundle.js
+++ hosted/loginBundle.js
@@ -147,3 +147,27 @@ var getToken = function getToken() {
 $(document).ready(function () {
   getToken();
 });
+"use strict";
+
+var handleError = function handleError(message) {
+  $("#errorMessage").text(message);
+};
+
+var redirect = function redirect(response) {
+  window.location = response.redirect;
+};
+
+var sendAjax = function sendAjax(type, action, data, success) {
+  $.ajax({
+    cache: false,
+    type: type,
+    url: action,
+    data: data,
+    dataType: "json",
+    success: success,
+    error: function error(xhr, status, _error) {
+      var messageObj = JSON.parse(xhr.responseText);
+      handleError(messageObj.error);
+    }
+  });
+};
