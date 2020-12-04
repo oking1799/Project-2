aa3af232a7ef7da9f943fe29fff77f6e108db551
Owen King <oking1799@gmail.com>
2020-12-04 14:43:30 -0500

    testing

--- server/controllers/location.js
+++ server/controllers/location.js
@@ -71,7 +71,7 @@ const getAllLocations = (request, response) => Location.LocationModel.find({}, (
 });
 
 const searchLocation = (request, response) => Location.LocationModel.find({ name: request.path }, (err, docs) => {
-  console.log("request is" + request.body);
+  console.log("request is:" + JSON.stringify(request.body));
 
   if (err) {
     console.log(err);
